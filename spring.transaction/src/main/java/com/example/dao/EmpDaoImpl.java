4f195694aafaf88639b649b2fc39fef750db1ad0
dineshmalik99 <dineshmalik99@gmail.com>
2018-04-04 14:39:47 +0530

    case when propagation level is Required and one transaction fails and even if exception is failed transaction gets roll back

--- spring.transaction/src/main/java/com/example/dao/EmpDaoImpl.java
+++ spring.transaction/src/main/java/com/example/dao/EmpDaoImpl.java
@@ -13,7 +13,7 @@ import com.example.util.EmployeeMapper;
 public class EmpDaoImpl extends JdbcDaoSupport implements EmpDao {
 
 	@Override
-	@Transactional(propagation=Propagation.REQUIRES_NEW)
+	@Transactional(propagation=Propagation.REQUIRED)
 	public void create(Employee emp) {
 		try {
 			String SQL = "INSERT INTO Employee (name, age, salary) VALUES (?, ?, ?)";
